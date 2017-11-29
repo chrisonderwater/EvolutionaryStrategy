4f0070b2bb3df17f2f7c1fce4a729ab7f96fa91c
Chris Onderwater <chrisonderwater@gmail.com>
2017-11-29 13:19:16 +0100

    Individual step sizes for each parameter.

--- mutate.m
+++ mutate.m
@@ -1,31 +1,34 @@
-function mutatedOffsprings = mutate(offsprings, bitMutations)
+function mutatedOffsprings = mutate(offsprings, bitMutations,LocalMutationRate,GlobalMutationRate,individualParameterStepsizes)
     mutatedOffsprings = offsprings;
     amountOffsprings = size(offsprings, 1);
     sizeOffspring = 30;
-   
+    
+    bitMutations = randi(5,1);
+    
     for i=1:amountOffsprings
         randomOffspringIndex = randi(amountOffsprings,1);
         currentOffspring = offsprings(randomOffspringIndex,:);
         for j=1:bitMutations
             randomMutationIndex = randi(sizeOffspring,1);
-            uniformNegativePositiveValue = 1 - 2*rand()
+            uniformNegativePositiveValue = 1 - 2*rand();
             
-            stepSize = 1
+            currentStepSize = individualParameterStepsizes(randomMutationIndex);
+            newStepSize = currentStepSize + exp(GlobalMutationRate * rand() + LocalMutationRate * rand());  
             
-            newParameterValue = currentOffspring(randomMutationIndex) + stepSize * uniformNegativePositiveValue;
+            newParameterValue = currentOffspring(randomMutationIndex) + newStepSize * uniformNegativePositiveValue;
        
        
             if newParameterValue > 1
-                newParameterValue = 1
+                newParameterValue = 1;
             end
             if newParameterValue < 0
-                newParameterValue = 0
+                newParameterValue = 0;
             end
             
-            mutatedOffspring = currentOffspring
+            mutatedOffspring = currentOffspring;
             mutatedOffspring(randomMutationIndex) = newParameterValue;
             
         end
-        mutatedOffsprings(randomOffspringIndex,:) = mutatedOffspring
+        mutatedOffsprings(randomOffspringIndex,:) = mutatedOffspring;
     end
 end
--- onderwater_maduro_es.m
+++ onderwater_maduro_es.m
@@ -2,14 +2,17 @@ function [xopt, fopt] = onderwater_maduro_es(eval_budget)
     population_size = 5;
     lambda = 100;   %amount of offsprings per generation
     bitMutations = 1;
+    individualParameterStepsizes = ones(30);
+    LocalLearningRate = 1/sqrt(2*sqrt(30));
+    GlobalLearningRate = 1/sqrt(2*30);
 
     t = 0;
-    population = initialize(population_size)
+    population = initialize(population_size);
     fitness = evaluate(population);
     fitnessEvolution = zeros(eval_budget,1);
     while (t < eval_budget)
         offsprings1 = recombine(population,lambda);
-        offsprings2 = mutate(offsprings1, bitMutations)
+        offsprings2 = mutate(offsprings1, bitMutations,LocalLearningRate,GlobalLearningRate,individualParameterStepsizes);
         [population, averageFitnessPopulation] = select(population, offsprings2);
         fitnessEvolution(t+1) = averageFitnessPopulation;
         t = t + 1;
