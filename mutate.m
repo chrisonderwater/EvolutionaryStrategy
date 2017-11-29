5b2400ea012e572ff881c835753af2d53b63a45e
Chris Onderwater <chrisonderwater@gmail.com>
2017-11-29 18:37:28 +0100

    Gregory: Fix bug in mutation

--- mutate.m
+++ mutate.m
@@ -3,19 +3,21 @@ function mutatedOffsprings = mutate(offsprings, bitMutations,LocalMutationRate,G
     amountOffsprings = size(offsprings, 1);
     sizeOffspring = 30;
     
-    bitMutations = randi(5,1);
+    bitMutations = randi(30,1);
     
     for i=1:amountOffsprings
         randomOffspringIndex = randi(amountOffsprings,1);
         currentOffspring = offsprings(randomOffspringIndex,:);
         for j=1:bitMutations
             randomMutationIndex = randi(sizeOffspring,1);
-            uniformNegativePositiveValue = 1 - 2*rand();
+            uniformNegativePositiveValue1 = 1 - 2*rand();
+            uniformNegativePositiveValue2 = 1 - 2*rand();
+            uniformNegativePositiveValue3 = 1 - 2*rand();
             
             currentStepSize = individualParameterStepsizes(randomMutationIndex);
-            newStepSize = currentStepSize + exp(GlobalMutationRate * rand() + LocalMutationRate * rand());  
+            newStepSize = currentStepSize + exp(GlobalMutationRate * uniformNegativePositiveValue1 + LocalMutationRate * uniformNegativePositiveValue2);  
             
-            newParameterValue = currentOffspring(randomMutationIndex) + newStepSize * uniformNegativePositiveValue;
+            newParameterValue = currentOffspring(randomMutationIndex) + newStepSize * uniformNegativePositiveValue3;
        
        
             if newParameterValue > 1
