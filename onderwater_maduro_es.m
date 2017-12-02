f06f5a40fe2315e1b12a217fd4c9d85702c24e7e
Chris Onderwater <chrisonderwater@gmail.com>
2017-12-02 14:43:16 +0100

    Added hyper parameter optimization for mu and lambda. Find best solution and its parameters

--- onderwater_maduro_es.m
+++ onderwater_maduro_es.m
@@ -1,23 +1,53 @@
 function [xopt, fopt] = onderwater_maduro_es(eval_budget)
-    population_size = 5;
-    lambda = 100;   %amount of offsprings per generation
-    bitMutations = 1;
+
     individualParameterStepsizes = ones(30);
     LocalLearningRate = 1/sqrt(2*sqrt(30));
     GlobalLearningRate = 1/sqrt(2*30);
-
-    t = 0;
-    population = initialize(population_size);
-    fitness = evaluate(population);
-    fitnessEvolution = zeros(eval_budget,1);
-    while (t < eval_budget)
-        offsprings1 = recombine(population,lambda);
-        offsprings2 = mutate(offsprings1, bitMutations,LocalLearningRate,GlobalLearningRate,individualParameterStepsizes);
-        [population, averageFitnessPopulation] = select(population, offsprings2);
-        fitnessEvolution(t+1) = averageFitnessPopulation;
-        t = t + 1;
+   
+    simulations = 2;
+    muLambdaSimulations = zeros(simulations,2);
+    minFopt = 1000;
+    minFoptMuLambdaSimulation = 0;
+    fitnessSimulations = zeros(simulations);
+    
+    for i=1:simulations
+        
+       
+        population_size = randi(50);
+        lambda = population_size + randi(50);   %amount of offsprings per generation
+        
+    
+        generations =  round(eval_budget/ (lambda * population_size)) ;
+        
+        t = 0;
+        population = initialize(population_size);
+        fitness = evaluate(population);
+        fitnessEvolution = zeros(generations,1);
+        while (t < generations)
+            offsprings1 = recombine(population,lambda);
+            offsprings2 = mutate(offsprings1,LocalLearningRate,GlobalLearningRate,individualParameterStepsizes);
+            [population, averageFitnessPopulation] = select(population, offsprings2);
+            fitnessEvolution(t+1) = averageFitnessPopulation;
+            t = t + 1;
+        end
+        fitnessEvolution
+        [fopt, xopt] = min(fitnessEvolution);
+        muLambdaSimulations(i, :) = [population_size lambda];
+        fitnessSimulations(i) = fopt;
+        if fopt < minFopt
+            minFopt = fopt;
+            minFoptPlotData = fitnessEvolution;
+            minFoptMuLambdaSimulation = [population_size lambda];
+        end
+        t
     end
     
-    plot(fitnessEvolution)
-    [fopt, xopt] = min(fitnessEvolution);
+    % Find the best simulation.
+    [minFopt, minFoptPos] = min(fitnessSimulations);
+    plot(minFoptPlotData)
+    minFoptMuLambdaSimulation
+    
+    %plot..
+    %plot(fitnessEvolution)
+    
 end
