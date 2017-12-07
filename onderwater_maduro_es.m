974df1aa4d169a1e607c6b74ad4d27de33aa9622
Gregory Maduro <gmmaduro@gmail.com>
2017-12-07 15:44:57 +0100

    trying to plot surface pretty. not working.

--- onderwater_maduro_es.m
+++ onderwater_maduro_es.m
@@ -4,34 +4,52 @@ function [xopt, fopt] = onderwater_maduro_es(eval_budget)
     LocalLearningRate = 1/sqrt(2*sqrt(30));
     GlobalLearningRate = 1/sqrt(2*30);
    
-    simulations = 50;
+    simulations = 50;       %20
     muLambdaSimulations = zeros(simulations,2);
-    minFopt = 1000;
+    minFopt = 999999999999;
     minFoptMuLambdaSimulation = 0;
-    fitnessSimulations = zeros(simulations);
+    fitnessSimulations = zeros(simulations,1);
+    
+    fitnessSurface = zeros(50,100)
+    
+    
+    %when we know optimal parameters use this part below..
+    %population_size = 5 %optimaal;            %
+    %lambda = 20 %optimaal;   %
+    %generations = floor( (eval_budget - population_size ) / lambda )
+    %fitnessSimulationsEvolution = zeros(generations,simulations)
     
     for i=1:simulations
+        population_size = randi(50)            %
+        lambda = population_size + randi(50)   %
+        generations = floor( (eval_budget - population_size ) / lambda )
         
-       
-        population_size = randi(50);
-        lambda = population_size + randi(50);   %amount of offsprings per generation
+        if generations <= 0
+            generations = 10
+        end
         
-    
-        generations =  floor( (eval_budget - population_size ) / lambda);
-        
-        t = 0;
+        t = 1;
         population = initialize(population_size);
         fitness = evaluate(population);
         fitnessEvolution = zeros(generations,1);
-        while (t < generations)
+        while (t <= generations)
             offsprings1 = recombine(population,lambda);
             offsprings2 = mutate(offsprings1,LocalLearningRate,GlobalLearningRate,individualParameterStepsizes);
             [population, averageFitnessPopulation] = select(population, offsprings2);
-            fitnessEvolution(t+1) = averageFitnessPopulation;
+            fitnessEvolution(t) = averageFitnessPopulation;
+            
+            %when we know optimal parameters
+            %fitnessSimulationsEvolution(t,i) = averageFitnessPopulation
+            
             t = t + 1;
         end
         fitnessEvolution
         [fopt, xopt] = min(fitnessEvolution);
+        
+        fitnessSurface(population_size,lambda) = fopt;
+        
+        
+        
         muLambdaSimulations(i, :) = [population_size lambda];
         fitnessSimulations(i) = fopt;
         if fopt < minFopt
@@ -42,11 +60,25 @@ function [xopt, fopt] = onderwater_maduro_es(eval_budget)
         t
     end
     
+    amountOfContours = 10  
+    contourf(fitnessSurface,amountOfContours)  %can crash if amount of contours to large
+  
     % Find the best simulation.
     [minFopt, minFoptPos] = min(fitnessSimulations);
-    plot(minFoptPlotData)
+    %plot(minFoptPlotData)
     minFoptMuLambdaSimulation
     minFopt
+    
+    %when we know optimal parameters use this part below..
+    %averageFitnessSimulations = mean(fitnessEvolution)
+    %stdFitnessSimulations = std(fitnessEvolution)
+    %meanFitnessEvolutionSimulations = mean(fitnessSimulationsEvolution,2)
+    %plot(meanFitnessEvolutionSimulations)
+    
+
+    
+    
+    
     
     
     %plot..
